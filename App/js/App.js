2a1da790d4255be080efab8c89b3d9925c88e3ac
Jerzy Rojek <jerzyadamrojek@gmail.com>
2020-08-17 17:16:56 +0200

    first commit

---
+++ App/js/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <h1>Hello App</h1>
+        //Header
+        //Sidebar
+        //Chat window - Router
+    )
+}
+
+export default App;
