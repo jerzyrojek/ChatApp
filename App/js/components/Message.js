6a4c172801336dbb962b45ca87e81aa7fd2970a3
Jerzy Rojek <jerzyadamrojek@gmail.com>
2020-08-28 15:05:21 +0200

    changed date and time display

--- App/js/components/Message.js
+++ App/js/components/Message.js
@@ -1,11 +1,14 @@
 import React from "react";
 
 const Message = ({message, timestamp, user}) => {
+    const date = new Date(timestamp?.toDate());
     return (
         <div className="message">
             <div className="message__info">
+                <span className="message__info-date">{date.toDateString()}</span>
+                <hr/>
                 <h3>
-                    {user} {new Date(timestamp?.toDate()).toLocaleTimeString()}
+                    {user} <span className="message__info-date">{date.toLocaleTimeString()}</span>
                 </h3>
                 <p>{message}</p>
             </div>
